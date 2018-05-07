1b9707ace26f369f31e6523651d6a470f5d0cb15
Jose L Riano <joselriano@yahoo.com>
2018-05-07 00:10:10 -0400

    code check

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ class App extends Component {
 
 	render(){
 
-		const videoSearch = _.debounce((term) => {this.videoSeach(term)}, 300);
+		const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
 
 		return (
 		<div>
